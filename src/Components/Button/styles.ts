e234cc93229d9a79dbaeaec5fec0a8e57597c11a
Matheus Barbieri <mbaraujo2@stefanini.com>
2020-07-29 19:22:55 -0300

    Changed theme style

--- src/Components/Button/styles.ts
+++ src/Components/Button/styles.ts
@@ -1,16 +1,8 @@
-import styled, { useTheme } from 'styled-components/native';
-import {variant} from 'styled-system'
+import styled from 'styled-components/native';
 import { TouchableOpacityProps } from 'react-native';
 
 interface IButton extends TouchableOpacityProps {
   variant: any
-}
-
-const getStyleByProps = (theme: any, variant: string, property: string) => {
-  if (theme.buttons[variant]) {
-    return theme.buttons[variant][property]
-  }
-  return false
 }
 
 export const ButtonDefault = styled.TouchableOpacity<IButton>`
@@ -21,11 +13,93 @@ export const ButtonDefault = styled.TouchableOpacity<IButton>`
   align-items: center;
   justify-content: center;
   margin: 18px 0;
-  background-color: ${({theme, variant}) => getStyleByProps(theme, variant, 'bg') || 'transparent'}
-  border: ${({theme, variant}) => getStyleByProps(theme, variant, 'border') || 'none'}
+  background-color: ${({ theme, variant }) => styles(theme)[variant]?.bg || 'transparent'}
+  border: ${({ theme, variant }) => styles(theme)[variant]?.border || 'none'}
 `;
 
 export const TextButton = styled.Text<IButton>`
-  color: ${({theme, variant}) => getStyleByProps(theme, variant, 'color') || '#000'}
+  color: ${({ theme, variant }) => styles(theme)[variant]?.color || '#000'}
   font-size: 18px;
 `
+
+function styles({ colors }: any) {
+  const theme = {
+    Primary: {
+      bg: colors.primary,
+      color: colors.white
+    },
+    Secondary: {
+      bg: colors.secondary,
+      color: colors.white
+    },
+    OutlinePrimary: {
+      bg: 'transparent',
+      border: `1px solid ${colors.primary}`,
+      color: colors.secondary
+    },
+    OutlineSecondary: {
+      bg: 'transparent',
+      border: `1px solid ${colors.secondary}`,
+      color: colors.primary
+    },
+    Ghost: {
+      color: colors.black
+    }
+  }
+
+  return theme
+}
+
+
+// function styles ({ colors, mode }: any) {
+//   console.log(mode);
+
+//   const theme = {
+//     light: {
+//       Primary: {
+//         bg: colors.primary,
+//         color: colors.white
+//       },
+//       Secondary: {
+//         bg: colors.secondary,
+//         color: colors.white
+//       },
+//       OutlinePrimary: {
+//         bg: 'transparent',
+//         border: `1px solid ${colors.primary}`,
+//         color: colors.secondary
+//       },
+//       OutlineSecondary: {
+//         bg: 'transparent',
+//         border: `1px solid ${colors.secondary}`,
+//         color: colors.primary
+//       },
+//       Ghost: {
+//         color: colors.black
+//       }
+//     },
+//     dark: {
+//       Primary: {
+//         bg: '#F0A6CA',
+//         color: '#9C89B8'
+//       },
+//       Secondary: {
+//         bg: '#B8BEDD',
+//         color: '#F0A6CA'
+//       },
+//       OutlinePrimary: {
+//         bg: 'transparent',
+//         border: '1px solid #F0A6CA'
+//       },
+//       OutlineSecondary: {
+//         bg: 'transparent',
+//         border: '1px solid #B8BEDD'
+//       },
+//       Ghost: {
+//         color: colors.black
+//       }
+//     }
+//   }
+
+//   return theme[mode]
+// }
